d3199ba4a0cbf26a71d1f12fb5dd46a35533ef89
Underline007 <99379498+Underline007@users.noreply.github.com>
2024-08-07 22:57:09 +0700

    Add project files.

---
+++ src/BaseProject.Domain/Common/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseProject.Domain.Common.Models
+{
+   public class BaseEntity
+    {
+        public virtual Guid Id { get; set; } = Guid.NewGuid();
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTimeOffset? LastModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
